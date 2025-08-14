65c990d807bc93ace34b84ab8cd75eaa4efb1f6c
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-08-13 19:32:45 -0600

    clase usuario

--- Act.py
+++ Act.py
@@ -5,6 +5,18 @@ class Libro:
         self.año = año
         self.codigoU = codigoU
 
-        def mostrar(self):
-            return (f"TITULO: {titulo} - AUTOR: {autor} - AÑO {año} - CODIGO: {codigoU}")
+        def mostrarL(self):
+            return (f"CODIGO: {codigo} - TITULO: {titulo} - AUTOR {autor} - AÑO: {año}")
+
+
+
+class Usuario:
+    def __init__(self,carnet,nombre,carrera):
+        self.carnet = carnet
+        self.nombre = nombre
+        self.carrera = carrera
+
+        def mostrarU(self):
+            return (f"CARNET: {carnet} - NOMBRE: {nombre} - CARRERA: {carrera}")
+
 
