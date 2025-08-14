398d9cfed991bb63832a4f7b9484ad24fae637f2
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-08-13 19:28:15 -0600

    clase libro

---
+++ Act.py
@@ -0,0 +1,10 @@
+class Libro:
+    def __init__(self,titulo,autor,año,codigoU):
+        self.titulo = titulo
+        self.autor = autor
+        self.año = año
+        self.codigoU = codigoU
+
+        def mostrar(self):
+            return (f"TITULO: {titulo} - AUTOR: {autor} - AÑO {año} - CODIGO: {codigoU}")
+
